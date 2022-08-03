b51d13f6d01b9c7a8ad5ec709d88415f1ba9edc9
yagoluis <yagoluisroberto@gmail.com>
2022-08-03 17:42:42 -0300

    Tela de login: layout e rota concluídos

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import './Css/reset.css';
+import './Css/style.css';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import Navbar from './Navbar';
+
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Login />}/>
+                <Route path='/cadastro' element={<Register />}/>
+            </Routes>
+        </BrowserRouter>
+    )
+    
+}
---
+++ src/components/Login.js
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+import { ReactComponent as Logo } from './Assets/TrackIt.svg'
+
+export default function Login() {
+    return (
+        <Page>
+            <Logo />
+            
+            <Form>
+                <input 
+                placeholder="E-mail"
+                type="text"
+                ></input>
+
+                <input 
+                placeholder="Senha"
+                type="text"
+                ></input>
+
+                <button type="submit">Entrar</button>
+            </Form>
+            
+            <span>Não tem uma conta? Cadastre-se!</span>
+        </Page>
+    )
+}
+
+const Page = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 70px;
+
+    span {
+        margin-top: 25px;
+        font-weight: 400;
+        font-size: 14px;
+        line-height: 18px;
+        color: #52B6FF;
+        text-decoration-line: underline;
+
+    }
+`
+
+const Form = styled.form`
+    width: 90%;
+    display: flex;
+    flex-direction: column;
+    margin-top: 33px;
+
+    input {
+        width: auto;
+        height: 45px;
+        padding-left: 11px;
+        margin-bottom: 8px;
+        background: #FFFFFF;
+        border: 1px solid #D5D5D5;
+        border-radius: 5px;
+        
+    }
+
+    button[type=submit] {
+        height: 45px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background: #52B6FF;
+        border-radius: 5px;
+        font-size: 21px;
+        font-weight: 400;
+        line-height: 26px;
+        color: #FFFFFF;
+        border: 0;
+        border: 1px solid #D5D5D5;
+        outline: unset;
+}
+`
+
+
+
+
